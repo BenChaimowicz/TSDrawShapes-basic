0ff09535d7f1dcf8acb08ea7614a0a59724dbb66
Ben Chaimo <doroza@hotmail.com>
2018-10-08 22:36:35 +0300

    Final

--- UIFunctions.js
+++ UIFunctions.js
@@ -28,35 +28,40 @@ function ShapeCreator() {
     }
 }
 function sizePrompt(hreq = false) {
-    let width = parseInt(prompt('Enter width: '));
-    while (width <= 0 || width == null) {
-        alert('ERROR! Number must be greater than 0!');
-        width = parseInt(prompt('Enter width: '));
+    let width = parseFloat(prompt('Enter width: '));
+    while (width <= 0 || width == null || Number.isInteger(width) != true) {
+        alert('ERROR! Number must be an INTEGER greater than 0!');
+        width = parseFloat(prompt('Enter width: '));
     }
     if (hreq == true) {
-        let height = parseInt(prompt('Enter Height: '));
-        while (height <= 0 || height == null) {
-            alert('ERROR! Number must be greater than 0!');
-            height = parseInt(prompt('Enter Height: '));
+        let height = parseFloat(prompt('Enter Height: '));
+        while (height <= 0 || height == null || Number.isInteger(height) != true) {
+            alert('ERROR! Number must be an INTEGER greater than 0!');
+            height = parseFloat(prompt('Enter Height: '));
         }
         return [width, height];
     }
     return [width];
 }
 function drawPrompt() {
-    let drawChoice = parseInt(prompt('Choose Drawing Style: \n 1. **** -Fill \n 2. *   * -Only borders \n 3. 1234 \n 4. 4321'));
-    switch (drawChoice) {
-        case 1:
-            return [true, DrawType.star];
-        case 2:
-            return [false, DrawType.star];
-        case 3:
-            return [true, DrawType.ascending];
-        case 4:
-            return [true, DrawType.descending];
-        default:
-            alert('ERROR! Input must be 1,2,3 or 4!');
-            throw new Error('Invalid Input Detected! Program stopped!');
-            break;
+    let drawChoice = parseFloat(prompt('Choose Drawing Style: \n 1. **** -Fill \n 2. *   * -Only borders \n 3. 1234 \n 4. 4321'));
+    if (Number.isInteger(drawChoice) == true) {
+        switch (drawChoice) {
+            case 1:
+                return [true, DrawType.star];
+            case 2:
+                return [false, DrawType.star];
+            case 3:
+                return [true, DrawType.ascending];
+            case 4:
+                return [true, DrawType.descending];
+            default:
+                alert('ERROR! Input must be 1,2,3 or 4!');
+                throw new Error('Invalid Input Detected! Program stopped!');
+                break;
+        }
+    }
+    else {
+        alert('Entry must be an integer!');
     }
 }
--- UIFunctions.ts
+++ UIFunctions.ts
@@ -30,17 +30,17 @@ function ShapeCreator() {
 }
 
 function sizePrompt(hreq: boolean = false): number[] {
-    let width: number = parseInt(prompt('Enter width: '));
+    let width: number = parseFloat(prompt('Enter width: '));
     
-    while (width <= 0 || width == null) {
-        alert('ERROR! Number must be greater than 0!');
-        width= parseInt(prompt('Enter width: '));
+    while (width <= 0 || width == null || Number.isInteger(width) != true) {
+        alert('ERROR! Number must be an INTEGER greater than 0!');
+        width= parseFloat(prompt('Enter width: '));
     }
     if (hreq == true) {
-        let height: number = parseInt(prompt('Enter Height: '));
-        while (height <= 0 || height == null) {
-            alert('ERROR! Number must be greater than 0!');
-            height = parseInt(prompt('Enter Height: '));
+        let height: number = parseFloat(prompt('Enter Height: '));
+        while (height <= 0 || height == null || Number.isInteger(height) != true) {
+            alert('ERROR! Number must be an INTEGER greater than 0!');
+            height = parseFloat(prompt('Enter Height: '));
         }
         return [width, height];
     }
@@ -48,19 +48,22 @@ function sizePrompt(hreq: boolean = false): number[] {
 }
 
 function drawPrompt():any[] {
-    let drawChoice: number = parseInt(prompt('Choose Drawing Style: \n 1. **** -Fill \n 2. *   * -Only borders \n 3. 1234 \n 4. 4321'));
-    switch (drawChoice) {
-        case 1:
-            return [true, DrawType.star];
-        case 2:
-            return [false, DrawType.star];
-        case 3:
-            return [true, DrawType.ascending];
-        case 4:
-            return [true, DrawType.descending];
-        default:
-            alert('ERROR! Input must be 1,2,3 or 4!');
-            throw new Error('Invalid Input Detected! Program stopped!');
-            break;
-    }
+    let drawChoice: number = parseFloat(prompt('Choose Drawing Style: \n 1. **** -Fill \n 2. *   * -Only borders \n 3. 1234 \n 4. 4321'));
+    
+    if (Number.isInteger(drawChoice) == true) {
+        switch (drawChoice) {
+            case 1:
+                return [true, DrawType.star];
+            case 2:
+                return [false, DrawType.star];
+            case 3:
+                return [true, DrawType.ascending];
+            case 4:
+                return [true, DrawType.descending];
+            default:
+                alert('ERROR! Input must be 1,2,3 or 4!');
+                throw new Error('Invalid Input Detected! Program stopped!');
+                break;
+        }
+    } else {alert('Entry must be an integer!')}
 }
